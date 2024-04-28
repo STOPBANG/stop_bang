793f28cbe659f5802bbd5c67737464d84007c373
plum-king <jeesoo0908@gmail.com>
2024-04-29 03:53:00 +0900

    feat: body 값 수정

--- controllers/realtorController.js
+++ controllers/realtorController.js
@@ -86,7 +86,7 @@ module.exports = {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: {r_username: res.locals.auth}
+      body: {r_id: r_id}
     }
     const forwardRequest = http.request(
       postOptions,
